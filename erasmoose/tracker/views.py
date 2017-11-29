14489cd23285deb2fa5bee38a35e9592237f16d5
JooeunAhn <jflow@hanyang.ac.kr>
2017-11-29 20:08:50 +0000

    modify stat

--- erasmoose/tracker/views.py
+++ erasmoose/tracker/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.db.models.functions import TruncMonth
+from django.db.models.functions import TruncMonth, TruncDay
 from django.db.models import Count
 
 from chartit import DataPool, Chart
@@ -23,14 +23,13 @@ def stat(request):
     today = datetime.datetime.now()
     total_count = Moose.objects.all().count()
     this_month = Moose.objects.filter(created_at__month=today.month).count()
-
     moose_list = \
-    Moose.objects.annotate(month=TruncMonth('created_at')).values('month').annotate(count=Count('id')).values('month', 'count')
+    Moose.objects.annotate(date=TruncDay('created_at')).values('date').annotate(count=Count('id')).values('date', 'count')
     moose_data = DataPool(series=[{
         "options": {
             'source': moose_list},
         "terms": [
-            'month',
+            'date',
             'count',
         ]
         }])
@@ -42,55 +41,16 @@ def stat(request):
                 "stacking": False
             },
             'terms': {
-                'month': ['count']
+                'date': ['count']
             },
         }],
-        chart_options={'title': {"text": "MOOSE ACCIDENT COUNT BY MONTH"}, "xAxis": {"title": {"text": "Month"}}})
+        chart_options={'title': {"text": "MOOSE ACCIDENT COUNT BY MONTH"}, "xAxis": {"title": {"text": "Date"}}})
 
     return render(request, "tracker/stat.html", {
         "total_count": total_count,
         "this_month": this_month,
         'line_chart': cht,
         })
-
-"""
-from chartit import DataPool, Chart
-
-def weather_chart_view(request):
-    #Step 1: Create a DataPool with the data we want to retrieve.
-    weatherdata = \
-        DataPool(
-           series=
-            [{'options': {
-               'source': MonthlyWeatherByCity.objects.all()},
-              'terms': [
-                'month',
-                'houston_temp',
-                'boston_temp']}
-             ])
-
-    #Step 2: Create the Chart object
-    cht = Chart(
-            datasource = weatherdata,
-            series_options =
-              [{'options':{
-                  'type': 'line',
-                  'stacking': False},
-                'terms':{
-                  'month': [
-                    'boston_temp',
-                    'houston_temp']
-                  }}],
-            chart_options =
-              {'title': {
-                   'text': 'Weather Data of Boston and Houston'},
-               'xAxis': {
-                    'title': {
-                       'text': 'Month number'}}})
-
-    #Step 3: Send the chart object to the template.
-    return render_to_response({'weatherchart': cht})
-"""
 
 
 @login_required
